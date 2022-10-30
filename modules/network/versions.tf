faf4efb812c171823bad98b6e3f45fff85b4aa71
Carlos Ramirez <carlosrv125@icloud.com>
2022-10-30 13:11:57 -0500

    corrected versions

--- modules/network/versions.tf
+++ modules/network/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2.5"
+  required_version = "~> 1.3.3"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.41.0"
+      version = "~> 4.41.0"
     }
   }
 }
