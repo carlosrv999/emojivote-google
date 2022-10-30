b30fcef5018733b5fcd8ec9186d3cd3cd1dd83e5
Carlos Ramirez <carlosrv125@icloud.com>
2022-10-30 13:08:22 -0500

    added vpc network

---
+++ modules/network/main.tf
@@ -0,0 +1,31 @@
+resource "google_compute_subnetwork" "public" {
+  count                    = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+  name                     = format("subnet-${var.public_subnet_suffix}-%v", count.index)
+  ip_cidr_range            = element(concat(var.public_subnets, [""]), count.index)
+  network                  = google_compute_network.default.id
+  stack_type               = "IPV4_ONLY"
+  private_ip_google_access = true
+  region                   = var.region
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+}
+
+resource "google_compute_subnetwork" "private" {
+  count                    = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+  name                     = format("subnet-${var.private_subnet_suffix}-%v", count.index)
+  ip_cidr_range            = element(concat(var.private_subnets, [""]), count.index)
+  network                  = google_compute_network.default.id
+  stack_type               = "IPV4_ONLY"
+  private_ip_google_access = true
+  region                   = var.region
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+}
