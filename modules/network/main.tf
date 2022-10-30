8e9680649d3a0f1562f17da21aac55b09e86048d
Carlos Ramirez <carlosrv125@icloud.com>
2022-10-30 13:13:35 -0500

    added network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,3 +1,10 @@
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+  mtu                     = 1460
+  routing_mode            = "REGIONAL"
+}
+
 resource "google_compute_subnetwork" "public" {
   count                    = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
   name                     = format("subnet-${var.public_subnet_suffix}-%v", count.index)
