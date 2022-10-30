b51e4496d634d96009fecaf436eff9f6cbaad5bb
Carlos Ramirez <carlosrv125@icloud.com>
2022-10-30 16:24:53 -0500

    fixed code

--- source/initdb/index.js
+++ source/initdb/index.js
@@ -1,10 +1,15 @@
 import * as mysql from 'mysql';
 import * as fs from 'fs'
-import { exit } from 'process';
+import * as process from 'process';
 
-const initializeDb = async () => {
+const initializeDb = () => {
+  console.log("environment variables are:", process.env.MYSQL_HOST, process.env.MYSQL_USER, process.env.MYSQL_PASSWD)
+  if (!process.env.MYSQL_HOST || !process.env.MYSQL_USER || !process.env.MYSQL_PASSWD) {
+    console.log("ERROR. Please set environment variables")
+    throw new Error("Environment variables have not been set");
+  }
   try {
-    const connection = mysql.createConnection({
+    const connection = new mysql.createConnection({
       host: process.env.MYSQL_HOST,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWD,
@@ -12,23 +17,26 @@ const initializeDb = async () => {
       charset: 'utf8mb4',
     })
 
-    connection.connect();
+    connection.connect((err) => {
+      if (err) {
+        console.log("connection error");
+      } else {
+        const sqlScript = fs.readFileSync('initialize.sql').toString();
+        console.log(sqlScript);
+        connection.query(sqlScript, (error, results) => {
+          if (error) throw error;
+          console.log("success, ", results);
+        })
 
-    const sqlScript = fs.readFileSync('initialize.sql').toString();
-    console.log(sqlScript);
-    const query = connection.query(sqlScript, (error, results) => {
-      if (error) throw error;
-      console.log("success, ", results);
-    })
+        connection.end();
 
-    console.log("success")
-    exit(0);
-
+        console.log("success")
+      }
+    });
   } catch (error) {
     console.log("an error happened: ", error)
     throw error;
   }
-
 }
 
 try {
