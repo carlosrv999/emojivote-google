8ba87e4935c37fe7544c7bf70e6a4f69e608031a
Carlos Ramirez <carlosrv125@icloud.com>
2022-10-30 14:39:18 -0500

    added dns and sql

--- main.tf
+++ main.tf
@@ -13,3 +13,22 @@ module "network" {
   region                = var.region
 
 }
+
+module "database" {
+  source = "./modules/database"
+
+  network_id               = module.network.network_id
+  cidr_block               = "10.100.32.0"
+  private_ip_address_name  = "private-sql-network"
+  cidr_block_prefix_length = 20
+  region                   = var.region
+  database_version         = "MYSQL_8_0"
+  home_ip_address          = "38.25.18.114"
+  instance_specs           = "db-n1-standard-2"
+}
+
+module "dns" {
+  source = "./modules/dns"
+
+  name        = "carlosrv125"
+}
